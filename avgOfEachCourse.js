d4e62d702ec6c55ea7c8e89dbb72e6b7bd5f9634
VaheStepanyan100 <84347859+VaheStepanyan100@users.noreply.github.com>
2022-07-03 16:46:33 +0400

    Update avgOfEachCourse.js

--- avgOfEachCourse.js
+++ avgOfEachCourse.js
@@ -1,40 +1,32 @@
-function avgOfEachCourse(evaluatins) {
-    let length = evaluatins.length;
-    let jsBoot = 0;
-    let jsLevel0 = 0;
-    let jsLevel1 = 0;
-    let obj = {
-        'JS Bootcamp': 0,
-        'JS Level 0': 0,
-        'JS Level 1': 0
-    };
-    let result = [];
+function groupBy(collection, key) {
+    const result = {};
 
-    for (let i = 0; i < length; i++) {
-        if (evaluatins[i].courseName === "JS Bootcamp") {
-            jsBoot++;
-            obj['JS Bootcamp'] += evaluatins[i].score
-        } else if (evaluatins[i].courseName === "JS Level 0") {
-            jsLevel0++;
-            obj['JS Level 0'] += evaluatins[i].score
-        } else if (evaluatins[i].courseName === "JS Level 1") {
-            jsLevel1++;
-            obj['JS Level 1'] += evaluatins[i].score
+    collection.forEach(element => {
+        if (result[element[key]]) {
+            result[element[key]] = [...result[element[key]], element]
+        } else {
+            result[element[key]] = [element];
         }
-    }
+    });
+    return result;
+}
 
+function calcAvg(arr, toFixed = 2) {
+    return (arr.reduce((acm, current) => acm + current) /
+     arr.length).toFixed(toFixed);
+}
 
-    let avg = {
-        'JS Bootcamp': obj['JS Bootcamp'] / jsBoot,
-        'JS Level 0': obj['JS Level 0'] / jsLevel0,
-        'JS Level 1': obj['JS Level 1'] / jsLevel1
-    }
-    
-    for (let key in avg) {
-        result.push({
-            courseName: key,
-            average: avg[key]
-        })
+function getAvgByCourse(evaluatins) {
+    const grouped = groupBy(evaluatins, "courseId");
+    const result = [];
+
+    for (let key in grouped) {
+        const courseId = grouped[key][0].courseId;
+        const courseName =grouped[key][0].courseName;
+        const scores = grouped[key].map(item => item.score);
+        const avg = calcAvg(scores, 1);
+
+        result.push({studentId: courseId, studentName: courseName, avg: avg})
     }
     return result;
 }
